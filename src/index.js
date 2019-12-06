75ca7f82d3678475a33632047d08f04ac3f12a72
Antonio Rodriguez <antoniwan@gmail.com>
2019-12-06 18:20:36 -0500

    added stanley or bd flags

--- src/index.js
+++ src/index.js
@@ -1,22 +1,35 @@
 const puppeteer = require("puppeteer");
+const argv = require("minimist")(process.argv.slice(2));
 const stanleySites = require("./data/stanleySites");
 const bdSites = require("./data/bdSites");
 const { checkSiteList } = require("./utils/misc");
 
-const main = async () => {
-  let counter = 1;
-  console.info(`Started running the headless-screenshots node app!`);
-
+const main = async list => {
   const todaysDate = new Date();
   const formattedTodaysDate = `${todaysDate.getFullYear()}_${todaysDate.getMonth() +
     1}_${todaysDate.getDate()}-`;
+  let urlList = null;
+  let counter = 1;
 
-  checkSiteList("stanleytools.global", stanleySites);
-  checkSiteList("blackandecker.global", bdSites);
+  switch (list) {
+    case "stanley":
+      checkSiteList("stanleytools.global", stanleySites);
+      urlList = stanleySites;
+      break;
+    case "bd":
+      checkSiteList("blackandecker.global", bdSites);
+      urlList = bdSites;
+      break;
+    default:
+      console.error(
+        "You must select a valid list option (--list stanley or --list bd)! "
+      );
+      process.exit();
+      break;
+  }
 
   const browser = await puppeteer.launch();
-
-  stanleySites.map(async site => {
+  urlList.map(async site => {
     const page = await browser.newPage();
     await page.setViewport({ width: 1200, height: 1024 });
     await page.goto(`https://${site}`, {
@@ -45,4 +58,9 @@ const main = async () => {
   });
 };
 
-main();
+console.info(
+  `This app will generate 🎑 of the current production SB&D Websites! The process will take several minutes, depending on the power of your 💻.\n`
+);
+
+const selectedList = argv.list;
+main(selectedList);
