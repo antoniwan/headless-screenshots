319f6fc01674a7eca4c64da5f8c07b3bbf75e57e
Antonio Rodriguez <antoniwan@gmail.com>
2019-12-06 14:43:37 -0500

    Project Init

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const puppeteer = require("puppeteer");
+const winston = require("winston");
+
+// create list of sites
+const stanleySites = require("./data/stanleySites");
+const bdSites = require("./data/bdSites");
+
+console.log(stanleySites, bdSites);
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto("https://ar.stanleytools.global/es");
+  await page.screenshot({ path: "screenshots/ar.stanleytools.global.png" });
+  await browser.close();
+})();
